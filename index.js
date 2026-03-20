32652fed3e8cb2247703d042bed2e3c48f0ce09f
Roberto Mazor <mazor.estudo@gmail.com>
2026-03-19 21:22:46 -0300

    fix rotas

--- index.js
+++ index.js
@@ -8,23 +8,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const mysql = require('mysql')
+const routerProdutos = require('./routes/produtosRoutes')
+const routerUsuarios = require('./routes/usuariosRoutes')
 
-let conexao = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
-
-conexao.connect(function (erro) {
-  if (erro) {
-    console.log("Deu ruim na conexão \n")
-    throw erro
-  } else {
-    console.log("Conexão deu bom \n")
-  }
-})
+app.use('/produtos', routerProdutos)
+app.use('/usuarios', routerUsuarios) 
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
